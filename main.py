e019a2285f87b7d110d31565c4559679a7c54355
Adam <aondr@disroot.org>
2025-08-04 12:00:59 +0200

    set different default origin url

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://elocity.neocities.org",
-    os.getenv("ORIGIN_URI", "https://elocity-dev.tadam.space")
+    os.getenv("ORIGIN_URI", "https://mpwapi.toaster.gay")
 ]
 
 app.add_middleware(
